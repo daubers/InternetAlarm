0132c840b9ee308f22494bc1ba743f3d3be64b26
Matt Daubney <matt@daubers.co.uk>
2017-06-11 20:01:31 +0100

    Add command line args stuff

--- internetalarm.py
+++ internetalarm.py
@@ -2,11 +2,13 @@ import pygame
 import pyping
 import time
 import datetime
+import argparse
 
-def play_sound():
+
+def play_sound(sound_file):
     pygame.init()
     pygame.mixer.init()
-    pygame.mixer.music.load('internets.mp3')
+    pygame.mixer.music.load(sound_file)
     clock = pygame.time.Clock()
     pygame.mixer.music.play()
     while pygame.mixer.music.get_busy():
@@ -15,12 +17,18 @@ def play_sound():
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("mp3file", type=str,
+                        help="Music file to play")
+    parser.add_argument("ping_location", type=str,
+                        help="Location to ping")
+    args = parser.parse_args()
     while True:
         try:
-            r = pyping.ping("8.8.8.2")
+            r = pyping.ping(args.ping_location)
             if r.ret_code == 0:
                 print("{0} INTERNETS!!!! :(".format(datetime.datetime.utcnow()))
-                play_sound()
+                play_sound(args.mp3file)
                 break
             else:
                 print("{0} No internets now :(".format(datetime.datetime.utcnow()))
